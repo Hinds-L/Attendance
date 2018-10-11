f89b9e471b5f9eabd44415a3460f2b78516adb63
Leslie <leslie.hinds82@gmail.com>
2018-10-11 12:05:55 -0600

    updated studentlistactivity, app, absence, studentdao

--- app/src/main/java/edu/cnm/deepdive/attendance/StudentDao.java
+++ app/src/main/java/edu/cnm/deepdive/attendance/StudentDao.java
@@ -1,8 +1,11 @@
 package edu.cnm.deepdive.attendance;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 import edu.cnm.deepdive.attendance.database.Student;
 import java.util.List;
 
@@ -18,6 +21,30 @@ public interface StudentDao {
   @Insert(onConflict = OnConflictStrategy.FAIL)
   List<Long> insert(List<Student> students);
 
-  //TODO add Update/Delete/Select
+  @Query("SELECT * FROM students ORDER BY last_name ASC, first_name ASC")
+  List<Student> select();
+
+  @Query("SELECT * FROM students"
+      + " WHERE last_name LIKE :pattern OR display_name LIKE :pattern"
+      + " ORDER BY last_name ASC, first_name ASC")
+  List<Student> select(String pattern);
+
+  @Update(onConflict = OnConflictStrategy.FAIL)
+  int update(Student student);
+
+  @Update(onConflict = OnConflictStrategy.FAIL)
+  int update(Student... students);
+
+  @Update(onConflict = OnConflictStrategy.FAIL)
+  int update(List<Student> student);
+
+  @Delete
+  int delete(Student student);
+
+  @Delete
+  int delete(Student... students);
+
+  @Delete
+  int delete(List<Student> student);
 
 }
